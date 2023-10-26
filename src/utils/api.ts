8baa85cfd633fca6751e133cb531e40a441d813d
Luis V <luisvinicius0906@gmail.com>
2023-10-26 14:53:07 -0300

    Update: axios instance to handle if user is unauthorized and setup token to request if exists

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,7 +1,45 @@
-import axios from 'axios';
+import axios, { AxiosError, AxiosRequestConfig, AxiosResponse } from 'axios';
+import { deleteCookie, getCookie } from 'cookies-next';
+import { redirect } from 'next/navigation';
 
 export const api = axios.create({
 	baseURL: process.env.NEXT_PUBLIC_BACKEND_URL,
 });
 
-// TODO: intercept response and handle if user is unauthorized
+const onResponse = (response: AxiosResponse) => {
+	return response;
+};
+
+const onResponseError = (error: AxiosError | Error) => {
+	if (error instanceof AxiosError) {
+		// TODO: Handle refresh token when available
+
+		if (error.response?.status === 401) {
+			if (typeof window === 'undefined') {
+				redirect('/api/handle-sign-out-cookie');
+			} else {
+				deleteCookie('token');
+				redirect('/login');
+			}
+		}
+	}
+
+	return Promise.reject(error);
+};
+
+api.interceptors.response.use(onResponse, onResponseError);
+
+api.interceptors.request.use(
+	(config) => {
+		const token = getCookie('token');
+
+		if (token) {
+			config.headers.Authorization = `Bearer ${token}`;
+		}
+
+		return config;
+	},
+	(err) => {
+		return Promise.reject(err);
+	}
+);
