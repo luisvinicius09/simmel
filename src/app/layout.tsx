92030c86e501d0925ae97f241755185d6d181760
Luis V <luisvinicius0906@gmail.com>
2023-11-02 10:31:47 -0300

    Add: geist font to root layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,9 @@
+import { GeistSans, GeistMono } from 'geist/font'
 import './globals.css';
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
 	return (
-		<html lang='en'>
+		<html lang='en' className={GeistSans.className}>
 			<body className='container bg-accent z-10'>{children}</body>
 		</html>
 	);
