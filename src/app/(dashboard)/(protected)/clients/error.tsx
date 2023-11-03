bc9c6938ebd93b8c4d65770752c4362c9e0f99a5
Luis V <luisvinicius0906@gmail.com>
2023-11-03 17:55:18 -0300

    Add: error component to clients page

---
+++ src/app/(dashboard)/(protected)/clients/error.tsx
@@ -0,0 +1,17 @@
+import Link from 'next/link';
+
+export default function Error({
+	error,
+	reset,
+}: {
+	error: Error & { digest?: string };
+	reset: () => void;
+}) {
+	return (
+		<div>
+			<h2>Something went wrong!</h2>
+
+			<Link href='/clients'>Go to Clients</Link>
+		</div>
+	);
+}
