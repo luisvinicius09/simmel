a3b89721ca729bfb17d27e2550d442f33c309db0
Luis V <luisvinicius0906@gmail.com>
2023-11-03 17:37:21 -0300

    Update: charge page by adding simple fetch function and error page

---
+++ src/app/(dashboard)/(protected)/charges/[chargeId]/error.tsx
@@ -0,0 +1,17 @@
+import Link from 'next/link';
+
+export default function Error({
+	error,
+	reset,
+}: {
+	error: Error & { digest?: string };
+	reset: () => void;
+}) {
+	return (
+		<div>
+			<h2>Charge do not exist or Something went wrong!</h2>
+
+			<Link href='/charges'>Go to Charges</Link>
+		</div>
+	);
+}
--- src/app/(dashboard)/(protected)/charges/[chargeId]/page.tsx
+++ src/app/(dashboard)/(protected)/charges/[chargeId]/page.tsx
@@ -1,12 +1,24 @@
-import Link from 'next/link'
+import { type ChargeDTO } from '@/shared/interfaces/charge.interface';
+import { api } from '@/utils/api';
+import Link from 'next/link';
 
-export default function Charge() {
-  return (
-    <div>
-      <Link href="/charges">Go back</Link>
-      <div>
-        <h1>Charge</h1>
-      </div>
-    </div>
-  )
+const getData = async (chargeId: string): Promise<ChargeDTO> => {
+	const res = await api.get<ChargeDTO>(`/charges/${chargeId}`);
+
+	return res.data;
+};
+
+export default async function Charge({ params }: { params: { chargeId: string } }) {
+	const data = await getData(params.chargeId);
+
+	return (
+		<div>
+			<Link href='/charges'>Go back</Link>
+
+			<div>
+				<h1>Charge ${data.id}</h1>
+        <p>Amount: ${data.amountInCents}</p>
+			</div>
+		</div>
+	);
 }
