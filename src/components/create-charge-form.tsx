c086a5e375318da6668aa33072506aad4e28de4f
Luis V <luisvinicius0906@gmail.com>
2023-11-11 10:04:46 -0300

    Update: create charge & client forms by improving it by adding fields, adding styling and connecting to back end

--- src/components/create-charge-form.tsx
+++ src/components/create-charge-form.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import { z } from 'zod';
 import { Button } from './ui/button';
 import { Calendar } from './ui/calendar';
 import { Checkbox } from './ui/checkbox';
@@ -15,89 +16,293 @@ import {
 	SelectTrigger,
 	SelectValue,
 } from './ui/select';
+import { useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
+import {
+	Form,
+	FormControl,
+	FormDescription,
+	FormField,
+	FormItem,
+	FormLabel,
+	FormMessage,
+} from './ui/form';
+import { format } from 'date-fns';
+import { ptBR } from 'date-fns/locale';
+import { Card, CardContent, CardHeader } from './ui/card';
+import { api } from '@/utils/api';
+import { IMaskMixin } from 'react-imask';
+import { Ref } from 'react';
 
-// userClientId: z.string(),
-// userProductId: z.string().nullable(),
-// amountInCents: z.number(),
-// expireDate: z.coerce.date().min(new Date()), // TODO: validation
-// selectedPaymentTypes: z.array(
-// 	z.enum([PaymentType.BOLETO, PaymentType.PIX, PaymentType.CREDIT_CARD]),
-// ),
+enum PaymentType {
+	BOLETO = 'BOLETO',
+	PIX = 'PIX',
+	CREDIT_CARD = 'CREDIT_CARD',
+}
+
+const createChargeFormSchema = z.object({
+	userClientId: z.string().nullable(),
+	userProductId: z.string().nullable(),
+	amountInCents: z.string(),
+	expireDate: z.coerce.date().min(new Date()),
+	selectedPaymentTypes: z.array(
+		z.enum([PaymentType.BOLETO, PaymentType.PIX, PaymentType.CREDIT_CARD])
+	),
+});
+
+type FormValues = z.infer<typeof createChargeFormSchema>;
 
 // TODO: fetch clients ids
 
+const MaskedInput = IMaskMixin(({ inputRef, ...props }) => (
+	<Input {...props} ref={inputRef as Ref<HTMLInputElement>} />
+));
+
 export default function CreateChargeForm() {
+	const form = useForm<FormValues>({
+		resolver: zodResolver(createChargeFormSchema),
+		defaultValues: {
+			selectedPaymentTypes: [],
+			userClientId: null,
+			userProductId: null,
+		},
+	});
+
+	function onSubmit(values: FormValues) {
+		api
+			.post('/charge/create', {
+				...values,
+				amountInCents: Number(values.amountInCents),
+			})
+			.then((res) => {
+				console.log(res.data);
+				return;
+			});
+	}
+
 	return (
 		<>
-			<form>
-				<fieldset>
-					<Input type='text' placeholder='Valor' />
-				</fieldset>
+			<Form {...form}>
+				<form onSubmit={form.handleSubmit(onSubmit)} className='flex flex-col gap-4'>
+					<Card>
+						<CardHeader />
 
-				<fieldset>
-					{/** TODO: https://ui.shadcn.com/docs/components/calendar */}
-					<Popover>
-						<PopoverTrigger asChild>
-							<Button variant={'outline'}>
-								<span>Selecione o Vencimento</span>
-							</Button>
-						</PopoverTrigger>
+						<CardContent className='flex flex-col gap-4'>
+							<FormField
+								control={form.control}
+								name='userProductId'
+								render={({ field }) => (
+									<FormItem className='w-full'>
+										<div className='flex flex-col'>
+											<FormLabel>Produto</FormLabel>
+											<FormDescription>Opcional</FormDescription>
+										</div>
 
-						<PopoverContent>
-							<Calendar />
-						</PopoverContent>
-					</Popover>
-				</fieldset>
+										<Select onValueChange={field.onChange}>
+											<FormControl>
+												<SelectTrigger>
+													<SelectValue placeholder='Selecione um produto' />
+												</SelectTrigger>
+											</FormControl>
 
-				<fieldset>
-					<Select>
-						<SelectTrigger className='w-[200px]'>
-							<SelectValue placeholder='Selecione um produto' />
-						</SelectTrigger>
+											<SelectContent>
+												<SelectGroup>
+													<SelectLabel>Produtos</SelectLabel>
+													<SelectItem value='1'>1</SelectItem>
+												</SelectGroup>
+											</SelectContent>
+										</Select>
 
-						<SelectContent>
-							<SelectGroup>
-								<SelectLabel>Produtos</SelectLabel>
-								<SelectItem value='1'>1</SelectItem>
-							</SelectGroup>
-						</SelectContent>
-					</Select>
-				</fieldset>
+										<FormMessage />
+									</FormItem>
+								)}
+							/>
 
-				<fieldset>
-					<Select>
-						<SelectTrigger className='w-[200px]'>
-							<SelectValue placeholder='Selecione um cliente' />
-						</SelectTrigger>
+							<FormField
+								control={form.control}
+								name='userClientId'
+								render={({ field }) => (
+									<FormItem className='w-full'>
+										<div className='flex flex-col'>
+											<FormLabel>Cliente</FormLabel>
+											<FormDescription>Opcional</FormDescription>
+										</div>
 
-						<SelectContent>
-							<SelectGroup>
-								<SelectLabel>Clientes</SelectLabel>
-								<SelectItem value='1'>1</SelectItem>
-							</SelectGroup>
-						</SelectContent>
-					</Select>
-				</fieldset>
+										<Select onValueChange={field.onChange}>
+											<FormControl>
+												<SelectTrigger>
+													<SelectValue placeholder='Selecione um cliente' />
+												</SelectTrigger>
+											</FormControl>
 
-				<fieldset>
-					<div>
-						<Checkbox id='boleto' />
-						<Label htmlFor='boleto'>Boleto</Label>
+											<SelectContent>
+												<SelectGroup>
+													<SelectLabel>Clientes</SelectLabel>
+													<SelectItem value='1'>1</SelectItem>
+												</SelectGroup>
+											</SelectContent>
+										</Select>
+
+										<FormMessage />
+									</FormItem>
+								)}
+							/>
+						</CardContent>
+					</Card>
+
+					<Card>
+						<CardHeader />
+
+						<CardContent className='flex flex-col gap-4'>
+							<FormField
+								control={form.control}
+								name='amountInCents'
+								render={({ field }) => (
+									<FormItem className='flex flex-col'>
+										<FormLabel>Valor da cobrança</FormLabel>
+
+										<FormControl>
+											<Input placeholder='Valor' {...field} />
+											{/* <MaskedInput placeholder='Valor' {...field} mask={Number} radix='.' /> */}
+										</FormControl>
+
+										{/* <FormDescription>Valor da cobrança</FormDescription> */}
+
+										<FormMessage />
+									</FormItem>
+								)}
+							/>
+
+							<FormField
+								control={form.control}
+								name='selectedPaymentTypes'
+								render={() => (
+									<FormItem className='flex flex-col'>
+										<FormLabel>Formas de Pagamento</FormLabel>
+										{/* <FormDescription>Valor da cobrança</FormDescription> */}
+
+										<FormField
+											control={form.control}
+											name='selectedPaymentTypes'
+											render={({ field }) => (
+												<FormItem className='flex flex-row space-x-2 space-y-0 items-center'>
+													<FormControl>
+														<Checkbox
+															checked={field.value?.includes(PaymentType.BOLETO)}
+															onCheckedChange={(checked) => {
+																return checked
+																	? field.onChange([...field.value, PaymentType.BOLETO])
+																	: field.onChange(
+																			field.value?.filter((value) => value !== PaymentType.BOLETO)
+																	  );
+															}}
+														/>
+													</FormControl>
+													<FormLabel className='text-sm font-normal'>
+														{PaymentType.BOLETO}
+													</FormLabel>
+												</FormItem>
+											)}
+										/>
+
+										<FormField
+											control={form.control}
+											name='selectedPaymentTypes'
+											render={({ field }) => (
+												<FormItem className='flex flex-row space-x-2 space-y-0 items-center'>
+													<FormControl>
+														<Checkbox
+															checked={field.value?.includes(PaymentType.CREDIT_CARD)}
+															onCheckedChange={(checked) => {
+																return checked
+																	? field.onChange([...field.value, PaymentType.CREDIT_CARD])
+																	: field.onChange(
+																			field.value?.filter(
+																				(value) => value !== PaymentType.CREDIT_CARD
+																			)
+																	  );
+															}}
+														/>
+													</FormControl>
+													<FormLabel className='text-sm font-normal'>
+														{PaymentType.CREDIT_CARD}
+													</FormLabel>
+												</FormItem>
+											)}
+										/>
+
+										<FormField
+											control={form.control}
+											name='selectedPaymentTypes'
+											render={({ field }) => (
+												<FormItem className='flex flex-row space-x-2 space-y-0 items-center'>
+													<FormControl>
+														<Checkbox
+															checked={field.value?.includes(PaymentType.PIX)}
+															onCheckedChange={(checked) => {
+																return checked
+																	? field.onChange([...field.value, PaymentType.PIX])
+																	: field.onChange(
+																			field.value?.filter((value) => value !== PaymentType.PIX)
+																	  );
+															}}
+														/>
+													</FormControl>
+													<FormLabel className='text-sm font-normal'>{PaymentType.PIX}</FormLabel>
+												</FormItem>
+											)}
+										/>
+
+										<FormMessage />
+									</FormItem>
+								)}
+							/>
+
+							<FormField
+								control={form.control}
+								name='expireDate'
+								render={({ field }) => (
+									<FormItem className='flex flex-col'>
+										<FormLabel>Vencimento</FormLabel>
+										<Popover>
+											<PopoverTrigger asChild>
+												<FormControl>
+													<Button variant={'outline'} className=''>
+														{field.value ? (
+															format(field.value, 'PPP', {
+																locale: ptBR,
+															})
+														) : (
+															<span>Selecione o vencimento</span>
+														)}
+													</Button>
+												</FormControl>
+											</PopoverTrigger>
+
+											<PopoverContent>
+												<Calendar
+													mode='single'
+													selected={field.value}
+													onSelect={field.onChange}
+													// disabled={(date) => date > new Date() || date < new Date('1900-01-01')}
+													initialFocus
+													locale={ptBR}
+												/>
+											</PopoverContent>
+										</Popover>
+
+										<FormMessage />
+									</FormItem>
+								)}
+							/>
+						</CardContent>
+					</Card>
+
+					<div className='flex justify-end'>
+						<Button type='submit'>Salvar</Button>
 					</div>
-					<div>
-						<Checkbox id='credit_card' />
-						<Label htmlFor='credit_card'>Cartão de Credito</Label>
-					</div>
-					<div>
-						<Checkbox id='pix' />
-						<Label htmlFor='pix'>Pix</Label>
-					</div>
-				</fieldset>
-
-				<div className='flex justify-end'>
-					<Button>Salvar</Button>
-				</div>
-			</form>
+				</form>
+			</Form>
 		</>
 	);
 }
--- src/components/create-client-form.tsx
+++ src/components/create-client-form.tsx
@@ -1,10 +1,23 @@
 'use client';
 
+import { z } from 'zod';
 import { Button } from './ui/button';
 import { Card, CardContent, CardDescription, CardHeader } from './ui/card';
 import { Input } from './ui/input';
 import { Label } from './ui/label';
 import { RadioGroup, RadioGroupItem } from './ui/radio-group';
+import { useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
+import {
+	Form,
+	FormControl,
+	FormDescription,
+	FormField,
+	FormItem,
+	FormLabel,
+	FormMessage,
+} from './ui/form';
+import { api } from '@/utils/api';
 
 // name         String
 // documentType DocumentType
@@ -21,94 +34,309 @@ import { RadioGroup, RadioGroupItem } from './ui/radio-group';
 // stateCode    String // UF
 // userClientId String
 
+const createClientFormSchema = z.object({
+	name: z.string(),
+	documentType: z.enum(['CPF', 'CNPJ']),
+	document: z.string(),
+	email: z.string().email(),
+	phoneNumber: z.string(),
+	address: z.object({
+		street: z.string(),
+		number: z.string(),
+		complement: z.string().optional(),
+		zipCode: z.string(),
+		city: z.string(),
+		district: z.string(),
+		stateCode: z.string(),
+	}),
+});
+
+type FormValues = z.infer<typeof createClientFormSchema>;
+
 export default function CreateClientForm() {
+	const form = useForm<FormValues>({
+		resolver: zodResolver(createClientFormSchema),
+		defaultValues: {
+			documentType: 'CPF',
+		},
+	});
+
+	function onSubmit(values: FormValues) {
+		api
+			.post('/client/create', {
+				...values,
+			})
+			.then((res) => {
+				console.log(res.data);
+				return;
+			});
+	}
+
 	return (
 		<>
-			<form className="flex flex-col gap-4">
-				<Card>
-					<CardHeader>
-						<h2>Informações Pessoais</h2>
-					</CardHeader>
+			<Form {...form}>
+				<form onSubmit={form.handleSubmit(onSubmit)} className='flex flex-col gap-4'>
+					<Card>
+						<CardHeader>
+							<h2>Informações Pessoais</h2>
+						</CardHeader>
 
-					<CardContent>
-						<div className="flex flex-col gap-5">
-							<fieldset>
-								<Input type='text' placeholder='Nome' />
-							</fieldset>
+						<CardContent>
+							<div className='flex flex-col gap-5'>
+								<FormField
+									control={form.control}
+									name='name'
+									render={({ field }) => (
+										<FormItem>
+											<FormLabel>Nome</FormLabel>
 
-							<fieldset>
-								<RadioGroup defaultValue='CPF' className="flex pb-2">
-									<div className="flex items-center">
-										<RadioGroupItem value='CPF' />
-										<Label>CPF</Label>
-									</div>
+											<FormControl>
+												<Input placeholder='Nome' {...field} />
+											</FormControl>
 
-									<div className="flex items-center">
-										<RadioGroupItem value='CNPJ' />
-										<Label>CNPJ</Label>
-									</div>
-								</RadioGroup>
+											{/* <FormDescription>Valor da cobrança</FormDescription> */}
 
-								<Input type='text' placeholder='Documento' />
-							</fieldset>
+											<FormMessage />
+										</FormItem>
+									)}
+								/>
 
-							<fieldset>
-								<Input type='email' placeholder='Email' />
-							</fieldset>
+								<FormField
+									control={form.control}
+									name='documentType'
+									render={({ field }) => (
+										<FormItem>
+											{/* <FormLabel>Nome</FormLabel> */}
 
-							<fieldset>
-								<Input type='number' placeholder='Celular' />
-							</fieldset>
-						</div>
-					</CardContent>
-				</Card>
+											<FormControl>
+												<RadioGroup
+													onValueChange={field.onChange}
+													defaultValue={field.value}
+													className='flex pb-2'
+												>
+													<FormItem className='flex items-center space-y-0 space-x-1'>
+														<FormControl>
+															<RadioGroupItem value='CPF' />
+														</FormControl>
+														<Label>CPF</Label>
+													</FormItem>
 
-				<Card>
-					<CardHeader>
-						<h2>Endereço</h2>
-					</CardHeader>
+													<FormItem className='flex items-center space-y-0 space-x-1'>
+														<FormControl>
+															<RadioGroupItem value='CNPJ' />
+														</FormControl>
+														<Label>CNPJ</Label>
+													</FormItem>
+												</RadioGroup>
+											</FormControl>
 
-					<CardContent>
-						<div className='flex gap-4 pb-5'>
-							<fieldset className='w-full'>
-								<Input type='text' placeholder='Rua' />
-							</fieldset>
+											{/* <FormDescription>Valor da cobrança</FormDescription> */}
 
-							<fieldset className='w-full'>
-								<Input type='text' placeholder='Numero' />
-							</fieldset>
-						</div>
+											<FormMessage />
+										</FormItem>
+									)}
+								/>
 
-						<div className='flex gap-4 pb-5'>
-							<fieldset className='w-full'>
-								<Input type='text' placeholder='Complemento' />
-							</fieldset>
+								<FormField
+									control={form.control}
+									name='document'
+									render={({ field }) => (
+										<FormItem>
+											<FormLabel>Documento</FormLabel>
 
-							<fieldset className='w-full'>
-								<Input type='text' placeholder='Bairro' />
-							</fieldset>
-						</div>
+											<FormControl>
+												<Input placeholder='Documento' {...field} />
+											</FormControl>
 
-						<div className='flex gap-4'>
-							<fieldset className='w-full'>
-								<Input type='text' placeholder='Zip Code' />
-							</fieldset>
+											{/* <FormDescription>Valor da cobrança</FormDescription> */}
 
-							<fieldset className='w-full'>
-								<Input type='text' placeholder='Cidade' />
-							</fieldset>
+											<FormMessage />
+										</FormItem>
+									)}
+								/>
 
-							<fieldset className='w-full'>
-								<Input type='text' placeholder='Estado' />
-							</fieldset>
-						</div>
-					</CardContent>
-				</Card>
+								<FormField
+									control={form.control}
+									name='email'
+									render={({ field }) => (
+										<FormItem>
+											<FormLabel>Email</FormLabel>
 
-				<div className='flex justify-end'>
-					<Button>Salvar</Button>
-				</div>
-			</form>
+											<FormControl>
+												<Input placeholder='Email' {...field} />
+											</FormControl>
+
+											{/* <FormDescription>Valor da cobrança</FormDescription> */}
+
+											<FormMessage />
+										</FormItem>
+									)}
+								/>
+
+								<FormField
+									control={form.control}
+									name='phoneNumber'
+									render={({ field }) => (
+										<FormItem>
+											<FormLabel>Celular</FormLabel>
+
+											<FormControl>
+												<Input placeholder='Celular' {...field} />
+											</FormControl>
+
+											{/* <FormDescription>Valor da cobrança</FormDescription> */}
+
+											<FormMessage />
+										</FormItem>
+									)}
+								/>
+							</div>
+						</CardContent>
+					</Card>
+
+					<Card>
+						<CardHeader>
+							<h2>Endereço</h2>
+						</CardHeader>
+
+						<CardContent>
+							<div className='flex gap-4 pb-5'>
+								<FormField
+									control={form.control}
+									name='address.street'
+									render={({ field }) => (
+										<FormItem className='w-full'>
+											<FormLabel>Rua</FormLabel>
+
+											<FormControl>
+												<Input placeholder='Rua' {...field} />
+											</FormControl>
+
+											{/* <FormDescription>Valor da cobrança</FormDescription> */}
+
+											<FormMessage />
+										</FormItem>
+									)}
+								/>
+
+								<FormField
+									control={form.control}
+									name='address.number'
+									render={({ field }) => (
+										<FormItem>
+											<FormLabel>Numero</FormLabel>
+
+											<FormControl>
+												<Input placeholder='Numero' {...field} />
+											</FormControl>
+
+											{/* <FormDescription>Valor da cobrança</FormDescription> */}
+
+											<FormMessage />
+										</FormItem>
+									)}
+								/>
+
+								<FormField
+									control={form.control}
+									name='address.complement'
+									render={({ field }) => (
+										<FormItem>
+											<FormLabel>Complemento</FormLabel>
+
+											<FormControl>
+												<Input placeholder='Complemento' {...field} />
+											</FormControl>
+
+											{/* <FormDescription>Valor da cobrança</FormDescription> */}
+
+											<FormMessage />
+										</FormItem>
+									)}
+								/>
+							</div>
+
+							<div className='flex gap-4'>
+								<FormField
+									control={form.control}
+									name='address.district'
+									render={({ field }) => (
+										<FormItem>
+											<FormLabel>Bairro</FormLabel>
+
+											<FormControl>
+												<Input placeholder='Bairro' {...field} />
+											</FormControl>
+
+											{/* <FormDescription>Valor da cobrança</FormDescription> */}
+
+											<FormMessage />
+										</FormItem>
+									)}
+								/>
+
+								<FormField
+									control={form.control}
+									name='address.zipCode'
+									render={({ field }) => (
+										<FormItem>
+											<FormLabel>Zip Code</FormLabel>
+
+											<FormControl>
+												<Input placeholder='Zip Code' {...field} />
+											</FormControl>
+
+											{/* <FormDescription>Valor da cobrança</FormDescription> */}
+
+											<FormMessage />
+										</FormItem>
+									)}
+								/>
+
+								<FormField
+									control={form.control}
+									name='address.city'
+									render={({ field }) => (
+										<FormItem>
+											<FormLabel>Cidade</FormLabel>
+
+											<FormControl>
+												<Input placeholder='Cidade' {...field} />
+											</FormControl>
+
+											{/* <FormDescription>Valor da cobrança</FormDescription> */}
+
+											<FormMessage />
+										</FormItem>
+									)}
+								/>
+
+								<FormField
+									control={form.control}
+									name='address.stateCode'
+									render={({ field }) => (
+										<FormItem>
+											<FormLabel>Estado</FormLabel>
+
+											<FormControl>
+												<Input placeholder='Estado' {...field} />
+											</FormControl>
+
+											{/* <FormDescription>Valor da cobrança</FormDescription> */}
+
+											<FormMessage />
+										</FormItem>
+									)}
+								/>
+							</div>
+						</CardContent>
+					</Card>
+
+					<div className='flex justify-end'>
+						<Button type='submit'>Salvar</Button>
+					</div>
+				</form>
+			</Form>
 		</>
 	);
 }
